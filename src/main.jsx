9a5f7cde493bb139567b5b0d987f62fda77ba46e
Alejandro Enciso <alejandro.encisof@gmail.com>
2023-01-05 18:42:32 -0600

    131.- Memo - Método de React

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,11 @@ import './index.css'
 // import {FormWithCustomHook} from "./02-useEffect/FormWithCustomHook.jsx";
 // import {MultipleCustomHooks} from "./03-examples/MultipleCustomHooks.jsx";
 // import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
-import {Layout} from "./05-useLayoutEffect/Layout.jsx";
+// import {Layout} from "./05-useLayoutEffect/Layout.jsx";
+import {Memorize} from "./06-memos/Memorize.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>,
 )
