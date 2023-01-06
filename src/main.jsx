afe0f2e41e557ff7fd7e4a0725c3b68fcb57bfb9
Alejandro Enciso <alejandro.encisof@gmail.com>
2023-01-05 20:00:43 -0600

    135.- Tarea Memorize

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,11 @@ import './index.css'
 // import {Layout} from "./05-useLayoutEffect/Layout.jsx";
 // import {Memorize} from "./06-memos/Memorize.jsx";
 // import {MemoHook} from "./06-memos/MemoHook.jsx";
-import {CallbackHook} from "./06-memos/CallbackHook.jsx";
+// import {CallbackHook} from "./06-memos/CallbackHook.jsx";
+import {Padre} from "./07-tarea-memo/Padre.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>,
 )
