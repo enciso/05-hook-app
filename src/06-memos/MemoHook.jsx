4243658f7f2e08ca413d2a8c9c254e2ddf574500
Alejandro Enciso <alejandro.encisof@gmail.com>
2023-01-05 18:53:23 -0600

    132.- useMemo

---
+++ src/06-memos/MemoHook.jsx
@@ -0,0 +1,40 @@
+import {useCounter} from "../hooks";
+import {useMemo, useState} from "react";
+
+const heavyStuff = ( iterationNumber = 100 ) => {
+    for (let i = 0; i < iterationNumber; i++) {
+        console.log('Ahí vamos')
+    }
+
+    return `${ iterationNumber } iteraciones realizadas`;
+}
+export const MemoHook = () => {
+
+    const { counter, increment } = useCounter( 4000 );
+    const [show, setShow] = useState(true);
+
+    const messageMemorizeValue = useMemo(() => heavyStuff(counter), [counter]);
+
+    return (
+        <>
+            <h1>Counter: <small>{ counter }</small></h1>
+            <hr/>
+
+            <h4>{ messageMemorizeValue }</h4>
+
+            <button
+                className="btn btn-primary"
+                onClick={ () => increment() }
+            >
+                +1
+            </button>
+
+            <button
+                className="btn btn-outline-primary"
+                onClick={ () => setShow( !show ) }
+            >
+                Show/Hide { JSON.stringify(show) }
+            </button>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,11 @@ import './index.css'
 // import {MultipleCustomHooks} from "./03-examples/MultipleCustomHooks.jsx";
 // import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
 // import {Layout} from "./05-useLayoutEffect/Layout.jsx";
-import {Memorize} from "./06-memos/Memorize.jsx";
+// import {Memorize} from "./06-memos/Memorize.jsx";
+import {MemoHook} from "./06-memos/MemoHook.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>,
 )
