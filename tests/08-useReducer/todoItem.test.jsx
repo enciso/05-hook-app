c699ac1e497b5e8e725afa1b44e79dd731c519dd
alejandro.enciso <alejandro.enciso@bbva.com>
2023-09-22 10:31:25 -0600

    todoItem.jsx

--- tests/08-useReducer/todoItem.test.jsx
+++ tests/08-useReducer/todoItem.test.jsx
@@ -1,4 +1,4 @@
-import {render,screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import {TodoItem} from "../../src/08-useReducer/TodoItem";
 
 describe('Pruebas con <TodoItem />', () => {
@@ -30,6 +30,57 @@ describe('Pruebas con <TodoItem />', () => {
         const spanElement = screen.getByLabelText('span');
         expect( spanElement.className ).toContain('align-self-center');
         expect( spanElement.className ).not.toContain('text-decoration-line-through');
+
+    });
+
+    test('debe de mostrar el Todo completado', () => {
+
+        todo.done = true;
+
+        render(
+            <TodoItem
+                item={ todo }
+                onToggleTodo={ onToggleTodoMock }
+                onDeleteTodo={ onDeleteTodoMock }
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        expect( spanElement.className ).toContain('text-decoration-line-through');
+
+    });
+
+    test('debe de mostrar el ToggleTodo cuando hace click', () => {
+
+        render(
+            <TodoItem
+                item={ todo }
+                onToggleTodo={ onToggleTodoMock }
+                onDeleteTodo={ onDeleteTodoMock }
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click( spanElement );
+
+        expect( onToggleTodoMock ).toHaveBeenCalledWith(todo.id);
+
+    });
+
+    test('debe de mostrar el ToggleTodo cuando hace click', () => {
+
+        render(
+            <TodoItem
+                item={ todo }
+                onToggleTodo={ onToggleTodoMock }
+                onDeleteTodo={ onDeleteTodoMock }
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click( spanElement );
+
+        expect( onToggleTodoMock ).toHaveBeenCalledWith(todo.id);
 
     });
 
